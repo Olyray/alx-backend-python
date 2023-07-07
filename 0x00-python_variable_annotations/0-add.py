6f20a7c147717cc1dbc44f25b75a06efba634905
Olamide <olire96@gmail.com>
2023-07-07 19:38:30 +0100

    Modified the file to adhere to the requirements

--- 0x00-python_variable_annotations/0-add.py
+++ 0x00-python_variable_annotations/0-add.py
@@ -1,6 +1,7 @@
-#!/usr/bin/python3
+#!/usr/bin/env python3
 """A function to add two floats"""
 
-def add (a: float, b: float) -> float:
+
+def add(a: float, b: float) -> float:
     """Add two floats"""
     return a + b
