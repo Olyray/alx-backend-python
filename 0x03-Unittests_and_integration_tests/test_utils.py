47304d769bef8d6ca4a7674a0dadd8b4e1db52f4
Olamide <olire96@gmail.com>
2023-07-31 23:10:13 +0100

    The third task

--- 0x03-Unittests_and_integration_tests/test_utils.py
+++ 0x03-Unittests_and_integration_tests/test_utils.py
@@ -3,7 +3,8 @@
 
 
 import unittest
-from utils import access_nested_map
+from utils import access_nested_map, get_json
+from unittest.mock import patch
 from parameterized import parameterized
 from typing import Dict, Tuple, Union, Mapping, Sequence
 
@@ -37,6 +38,19 @@ class TestAccessNestedMap(unittest.TestCase):
         """Tests that a key error is returned"""
         with self.assertRaises(KeyError):
             access_nested_map(nested_map, path)
+
+
+class TestGetJson(unittest.TestCase):
+    @parameterized.expand([
+        ("http://example.com", {"payload": True}),
+        ("http://holberton.io", {"payload": False})
+    ])
+    @patch('utils.requests.get')
+    def test_get_json(self, test_url, test_payload, mock_get):
+        mock_get.return_value.json.return_value = test_payload
+        response = get_json(test_url)
+        mock_get.assert_called_once_with(test_url)
+        self.assertEqual(response, test_payload)
 
 
 if __name__ == "__main__":
