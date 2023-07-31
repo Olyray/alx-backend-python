a374d70d46dff34e0d35d6fb88ea29813306e832
Olamide <olire96@gmail.com>
2023-07-31 20:02:35 +0100

    Implementation of the first task

--- 0x03-Unittests_and_integration_tests/README.md
+++ 0x03-Unittests_and_integration_tests/README.md
@@ -1,2 +1,4 @@
 This directory contains files for the project unittests and integration tests. These are the files in the directory:
 
+0) utils.py: Generic utilities for github org client.
+1) test_utils.py: Unit test for utils.py
--- 0x03-Unittests_and_integration_tests/test_utils.py
+++ 0x03-Unittests_and_integration_tests/test_utils.py
@@ -1,18 +1,24 @@
 #!/usr/bin/env python3
+"""Unit test for utils.py"""
+
+
 import unittest
-from parameterized import parameterized
 from utils import access_nested_map
+from parameterized import parameterized
+
 
 class TestAccessNestedMap(unittest.TestCase):
-
+    """This class tests access_nested_map"""
     @parameterized.expand([
         ({"a": 1}, ("a",), 1),
         ({"a": {"b": 2}}, ("a",), {"b": 2}),
-        ({"a": {"b": 2}}, ("a", "b"), 2),
+        ({"a": {"b": 2}}, ("a", "b"), 2)
     ])
-    def test_access_nested_map(self, nested_map, path, expected_result):
-        result = access_nested_map(nested_map, path)
-        self.assertEqual(result, expected_result)
+    def test_access_nested_map(self, nested_map, path, result):
+        """This method runs a test for access_nested_map"""
+        returned_element = access_nested_map(nested_map, path)
+        self.assertEqual(result, returned_element)
+
 
 if __name__ == "__main__":
     unittest.main()
